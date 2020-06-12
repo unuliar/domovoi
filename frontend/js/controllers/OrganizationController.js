917d2c502d5771d2005d2908ccc7f6a4c1d2c4ca
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2020-06-12 16:40:50 +0300

    Frontend - backend link + CORS fix

--- frontend/js/controllers/OrganizationController.js
+++ frontend/js/controllers/OrganizationController.js
@@ -1,3 +1,16 @@
 app.controller('OrganizationController', function ($scope, $rootScope) {
+    $scope.currentOrg = null;
+
+    $rootScope.apiCall(
+        'GET',
+        'organisation/getByToken',
+        {},
+        (result) => {
+            if(result.data.status === 'ok') {
+                $scope.currentOrg = result.data.org;
+            }
+        }
+    );
+
     $rootScope.pageTitle = "Управляющая компания";
 });
