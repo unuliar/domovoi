de44e1e907b402193f2e396c6a610affd70614b9
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2020-06-13 11:18:10 +0300

    testing meeting api

--- frontend/js/controllers/MeetingsRoomController.js
+++ frontend/js/controllers/MeetingsRoomController.js
@@ -4,6 +4,31 @@ app.controller('MeetingsRoomController', function ($scope, $rootScope, $routePar
 
     $rootScope.pageTitle = `Собрание №${$routeParams.meeting_id}`;
 
+    $scope.messages = [];
+
+    $scope.currentMeeting = {
+        title: "Решаем важные вопросы",
+        date: "22 Мая, 2020 - 18:00",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra facilisis ullamcorper. Praesent porta leo ac sem bibendum sollicitudin. Fusce finibus, nibh id interdum consequat, ipsum metus varius augue, vel semper eros velit a urna. Nunc feugiat felis ut accumsan vulputate. Cras sit amet nulla id sem eleifend rhoncus. Etiam sollicitudin a sem vel lobortis. Aenean mollis malesuada purus eget porta. Cras sagittis ante a metus semper condimentum. Praesent vel tellus maximus, cursus sem vitae, blandit purus. Ut a mi a libero efficitur tincidunt sed a quam. Nulla cursus nisi in rutrum malesuada. Cras varius dui ac ante egestas sollicitudin. Integer congue est nec ipsum facilisis consequat. Proin a tellus enim. Nullam et erat ut nulla tempus condimentum.\n" +
+            "\n" +
+            "Praesent iaculis mauris nisl, id faucibus massa bibendum vel. Donec aliquam leo at suscipit ornare. Suspendisse enim metus, iaculis et dui in, tempus semper dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas placerat diam sit amet erat convallis vestibulum. Duis ut lorem vehicula felis ultricies accumsan. Mauris in bibendum nisl. In volutpat dignissim ex et posuere. Suspendisse potenti. Nunc eu urna et sem maximus gravida a vel mauris.",
+        polls: [
+            {
+                id: 1,
+                question: "Вы хотите новую детскую площадку?"
+            },
+            {
+                id: 2,
+                question: "Вы хотите новую лавочку у подьезда?"
+            },
+            {
+                id: 3,
+                question: "Вы хотите новую машину?"
+            }
+        ],
+        attachments: []
+    };
+
     $rootScope.apiCall(
         'GET',
         'meeting/get',
@@ -11,7 +36,7 @@ app.controller('MeetingsRoomController', function ($scope, $rootScope, $routePar
             id: $routeParams.meeting_id
         },
         (result) => {
-            console.log(result);
+            $scope.messages = result.data.meeting.chat_messages;
         }
     );
     /** Connection */
@@ -24,7 +49,7 @@ app.controller('MeetingsRoomController', function ($scope, $rootScope, $routePar
     $scope.socket = new WebSocket(wsFormat);
 
     $scope.messageBox = '';
-    $scope.messages = [];
+
 
     $scope.socket.onmessage = (event) => {
         console.log(event);
@@ -71,29 +96,4 @@ app.controller('MeetingsRoomController', function ($scope, $rootScope, $routePar
     };
 
     $scope.params = $routeParams;
-
-    $scope.currentMeeting = {
-        title: "Решаем важные вопросы",
-        date: "22 Мая, 2020 - 18:00",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra facilisis ullamcorper. Praesent porta leo ac sem bibendum sollicitudin. Fusce finibus, nibh id interdum consequat, ipsum metus varius augue, vel semper eros velit a urna. Nunc feugiat felis ut accumsan vulputate. Cras sit amet nulla id sem eleifend rhoncus. Etiam sollicitudin a sem vel lobortis. Aenean mollis malesuada purus eget porta. Cras sagittis ante a metus semper condimentum. Praesent vel tellus maximus, cursus sem vitae, blandit purus. Ut a mi a libero efficitur tincidunt sed a quam. Nulla cursus nisi in rutrum malesuada. Cras varius dui ac ante egestas sollicitudin. Integer congue est nec ipsum facilisis consequat. Proin a tellus enim. Nullam et erat ut nulla tempus condimentum.\n" +
-            "\n" +
-            "Praesent iaculis mauris nisl, id faucibus massa bibendum vel. Donec aliquam leo at suscipit ornare. Suspendisse enim metus, iaculis et dui in, tempus semper dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas placerat diam sit amet erat convallis vestibulum. Duis ut lorem vehicula felis ultricies accumsan. Mauris in bibendum nisl. In volutpat dignissim ex et posuere. Suspendisse potenti. Nunc eu urna et sem maximus gravida a vel mauris.",
-        polls: [
-            {
-                id: 1,
-                question: "Вы хотите новую детскую площадку?"
-            },
-            {
-                id: 2,
-                question: "Вы хотите новую лавочку у подьезда?"
-            },
-            {
-                id: 3,
-                question: "Вы хотите новую машину?"
-            }
-        ],
-        attachments: []
-    };
-
-
 });
