827af3450ce637dd32a08c8f38c0fd615def4d9d
np <np@i7.org>
2020-06-14 08:01:02 +0300

    Api attachments

--- frontend/js/controllers/OrganizationController.js
+++ frontend/js/controllers/OrganizationController.js
@@ -12,6 +12,7 @@ app.controller('OrganizationController', function ($scope, $rootScope) {
         (result) => {
             if(result.data.status === 'ok') {
                 $scope.currentOrg = result.data.org;
+                $scope.currentOrg.respect_index=  $scope.currentOrg.respect_index*100;
                 $rootScope.setLoader(false);
             }
         }
