94d1e19bcd5b5d0039546a24658e5ffff9d6a994
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2020-06-14 09:15:32 +0300

    Frontend update

--- frontend/js/controllers/RatingController.js
+++ frontend/js/controllers/RatingController.js
@@ -12,6 +12,7 @@ app.controller('RatingController', function ($scope, $rootScope) {
         'organisations/get',
         {},
         (result) => {
+            console.log(result)
             $rootScope.setLoader(false);
             $scope.organisations = result.orgs;
         }
